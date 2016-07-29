39cd19d8363c29ec3afb6df94349529e936c5a74
neal-o-r <neal@n-o-r.xyz>
2016-07-29 23:26:03 +0100

    cnn stuff added

--- data_read.py
+++ data_read.py
@@ -5,9 +5,8 @@ def file_string(filename):
 
 	with open(filename, 'r') as f:
 
-		return f.read()
+		return f.read()		
 
-		
 
 def data_and_labels(filenames):
 
@@ -15,7 +14,7 @@ def data_and_labels(filenames):
 	cut = 100
 
 	texts  = np.zeros((0,200)) 
-	labels = np.zeros(0)
+	labels = []
 	for no, filename in enumerate(filenames):
 
 		f_str = file_string(filename)
@@ -25,10 +24,30 @@ def data_and_labels(filenames):
 		char_arr = char_arr.reshape((len(char_arr)+1)//n, n)		
 		#char_arr = char_arr[:cut]
 		
-		lab = np.zeros(char_arr.shape[0]) + no
-
+		lab = [[no, int(not(no))] for i in range(char_arr.shape[0])]
 		texts = np.vstack((texts, char_arr))
-		labels = np.concatenate((labels, lab))	
-		
-
+		labels.extend(lab)	
+			
+	
 	return texts, labels
+
+def batch_iter(data, batch_size, num_epochs, shuffle=True):
+    """
+    Generates a batch iterator for a dataset.
+    """
+    data = np.array(data)
+    data_size = len(data)
+    num_batches_per_epoch = int(len(data)/batch_size) + 1
+    for epoch in range(num_epochs):
+        # Shuffle the data at each epoch
+        if shuffle:
+            shuffle_indices = np.random.permutation(np.arange(data_size))
+            shuffled_data = data[shuffle_indices]
+        else:
+            shuffled_data = data
+        for batch_num in range(num_batches_per_epoch):
+            start_index = batch_num * batch_size
+            end_index = min((batch_num + 1) * batch_size, data_size)
+            yield shuffled_data[start_index:end_index]
+
+
