dc632c40caca2d67e6eeb9ed9a1ab4b779030136
neal-o-r <neal@n-o-r.xyz>
2016-08-11 19:30:07 +0100

    added extns to downloader

--- ddata/gthb_dwld.py
+++ ddata/gthb_dwld.py
@@ -3,22 +3,23 @@ import github as gthb
 inst = gthb.Github()
 
 
-languages = ['Python', 'PHP', 'Ruby', 'Javascript', 'Haskell']
-
+languages  = ['Python', 'PHP', 'Ruby', 'Javascript', 'Haskell']
+extensions = ['.py', '.php', '.rb', '.js', '.hs']
 
 with open('urls.txt','w') as f:
-	for lang in languages:	
+
+	for ind, lang in enumerate(languages):	
 		repos = inst.search_repositories(lang, language=lang, sort='stars', order='desc')
 
 		size = 0
 		i = 0
-		while size < 1e3:
+		while size < 3e5:
 
 			url = repos[i].html_url	
 			size += repos[i].size
 			i+=1
 
-			f.write(lang+','+url+'\n')	
+			f.write(lang+','+url+','+extensions[ind]+'\n')	
 
 
 
